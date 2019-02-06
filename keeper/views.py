bd13f16b96c61f7b542c03328dcfa710295fcd9d
Krotyuk <krotyuk.nikolay@gmail.com>
2019-02-06 15:03:30 +0700

    create access_detail

--- keeper/views.py
+++ keeper/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import accessPost
+from django.shortcuts import render, get_object_or_404
+
+
 
 # Create your views here.
 def access_list(request):
-    return render(request, 'keeper/access_list.html', {})
+    listOfAccess = list(accessPost.objects.all())
+
+
+    return render(request, 'keeper/access_list.html', {'listOfAccess': listOfAccess})
+
+def access_detail(request, pk):
+    access_detail = get_object_or_404(accessPost, pk=pk)
+    return render(request, 'keeper/access_detail.html', {'access_detail': access_detail})
